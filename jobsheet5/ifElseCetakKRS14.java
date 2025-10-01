e89c7c9e639115cd636821be29365345fb8e9b15
mohhamadgiovandisaputra-glitch <mohhamadgiovandisaputra@gmail.com>
2025-10-01 09:09:45 +0700

    Modifikasi Percobaan 2

---
+++ jobsheet5/ifElseCetakKRS14.java
@@ -0,0 +1,40 @@
+package jobsheet5;
+import java.util.Scanner;
+public class ifElseCetakKRS14 {
+
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+
+        System.out.println("---Cetak KRS SIAKAD---");
+        System.out.println("Masukan Semester Saat Ini");
+        int semester =sc.nextInt();
+
+        if (semester == 1){
+            System.out.println("KRS Semester 1 Ditampilkan");
+
+        }else if (semester == 2){
+            System.out.println("KRS Semester 2 Ditampilkan");
+
+        }else if (semester == 3){
+            System.out.println("KRS Semester 3 Ditampilkan");
+
+        }else if (semester == 4){
+            System.out.println("KRS Semester 4 Ditampilkan");
+
+        }else if (semester == 5){
+            System.out.println("KRS Semester 5 Ditampilkan");
+
+        }else if(semester == 6){
+            System.out.println("KRS Semester 6 Ditampilkanv");
+
+        }else if(semester == 7){
+            System.out.println("KRS Semester 7 Ditampilkan");
+
+        }else if(semester == 8){
+            System.out.println("KRS Semester 8 Ditampilkan");
+
+        }else{
+            System.out.println("Semester Tidak Valid");
+        }
+    }
+}
