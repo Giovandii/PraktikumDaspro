11371076f3c8a37535b69b5a2ef0bc09d19c234a
mohhamadgiovandisaputra-glitch <mohhamadgiovandisaputra@gmail.com>
2025-10-15 11:13:48 +0700

    perbaikan

--- UTS/Menentukanangkaterbesar_14.java
+++ UTS/Menentukanangkaterbesar_14.java
@@ -11,14 +11,24 @@ public class Menentukanangkaterbesar_14 {
 
        
 
-       if (angka1 > angka2 || angka1 > angka3 ){
+       if (angka1 > angka2 && angka1 > angka3 ){
             System.out.println("Angka terbesar adalah : " +angka1); 
-        }else if (angka2 > angka1 || angka2 > angka3 ){
+        }else if (angka2 > angka1 && angka2 > angka3 ){
             System.out.println("Angka terbesar adalah : " +angka2); 
-        }else if (angka3 > angka2 || angka3 > angka2 ){
-            System.out.println("Angka terbesar adalah : " +angka3); 
         }else {
-            System.out.println("Ada beberapa angka yang sama : " +angka1 +angka2 +angka3);
+            System.out.println("Angka terbesar adalah : " +angka3); 
+        }
+
+        if (angka1==angka2 && angka1>angka3){
+            System.out.println("Nilai Yang Sama Besar " +angka1 +" dan "+angka2);
+        }else if (angka2 == angka3 && angka2 > angka1){
+             System.out.println("Nilai Yang Sama Besar " +angka2 +" dan "+angka3);
+        }else if (angka3 == angka1 && angka3>angka2){
+             System.out.println("Nilai Yang Sama Besar " +angka1 +" dan "+angka3);
+        }else if (angka1 == angka2 && angka1 == angka3){
+             System.out.println("Nilai Yang Sama Besar " +angka1 +","+angka3+" dan "+angka2);
+        }else{
+            System.out.println("Angka Tidak Ada yang sama");
         }
     }
 }
